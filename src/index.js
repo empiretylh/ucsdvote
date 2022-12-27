df6557634dfb22d72a86a625e71d5d6dca18ae68
Thura Lin Htut <thuralinhtut3@gmail.com>
2022-12-27 15:08:29 +0630

    27 DEC change

--- src/index.js
+++ src/index.js
@@ -3,13 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import { HashRouter } from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+<HashRouter>
     <App />
+    </HashRouter>
   </React.StrictMode>
 );
 
