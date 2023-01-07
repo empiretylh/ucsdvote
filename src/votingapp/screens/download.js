9b53f6eaeca7fc9ad7e1ec0447478d48fe103250
Thura Lin Htut <thuralinhtut3@gmail.com>
2023-01-07 16:51:14 +0000

    download

--- src/votingapp/screens/download.js
+++ src/votingapp/screens/download.js
@@ -1,5 +1,6 @@
 import React from "react";
-
+import { Container, Row, Col, Button } from "react-bootstrap";
+import { Android2,QuestionCircle } from "react-bootstrap-icons";
 import { IMAGE as I } from "../../assets/assets";
 const Download = () => {
   return (
@@ -10,15 +11,48 @@ const Download = () => {
         minHeight: "100vh",
       }}
     >
-      <div>
-        <h1>Choose your King and Queen Today</h1>
-        <p>Remember to vote Bhone Nay Lin</p>
-
-      </div>
-      <div>
-         <img src={I.ph1}/>
-          <img src={I.ph2}/>
-      </div>
+      <Container>
+        <Row>
+          <Col
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              height: "100vh",
+            }}
+          >
+            <div style={{ display: "flex", flexDirection: "column" }}>
+              <h1 style={{fontFamily:'Roboto-Bold'}}>Choose Your King and Queen Today.</h1>
+              <p style={{fontFamily:'Roboto-Regular'}}>
+                Remember to vote Bhone Nay Lin. This app is only avaible for
+                andriod We are working for IOS, too. Thank you for your
+                understanding.
+              </p>
+              <div>
+                <Button variant="primary" onClick={()=>{
+                  window.open('https://github.com/empiretylh/empire/raw/main/Voting/UCSD%20Voting.apk');
+                }}>
+                  <Android2 size={20} style={{marginRight:8}}/>Download App</Button>
+                  <Button variant="outline-black" style={{marginLeft:10}} onClick={()=>{
+                  // window.open('https://github.com/empiretylh/empire/raw/main/Voting/UCSD%20Voting.apk');
+                }}>
+                  <QuestionCircle size={20} style={{marginRight:8}}/>How to Vote?</Button>
+              </div>
+            </div>
+          </Col>
+          <Col
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              height: "100vh",
+            }}
+          >
+            <img src={I.ph1} style={{ width: 300 }} />
+            <img src={I.ph2} style={{ width: 200 }} />
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 };
